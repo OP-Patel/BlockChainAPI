7aae93d97d6b8b03bc5b255514909ccbdf1d6ca6
Om Patel <ompatel@Oms-MacBook-Pro.local>
2023-07-30 17:47:51 -0400

    Finish backend functionality and API link

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 
 const app = express(); 
 const port = 3000; 
+const API_URL = "https://api.blockchain.com/v3/exchange"
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,6 +14,25 @@ app.get("/", (req, res) => {
     res.render("index.ejs");
   });
 
+app.post("/submit", async (req, res)=>{
+    var userInput = req.body["symbolName"];
+    try {
+      var response = await axios.get(API_URL+"/tickers/"+userInput);
+      var symbo = response.data.symbol;
+      var price_24 = response.data.price_24h;
+      var vol_24 = response.data.volume_24h;
+      var lastTradePrice = response.data.last_trade_price; 
+      res.render("submit.ejs", {
+          symbol: symbo,
+          price: price_24,
+          volume: vol_24,
+          lastPrice: lastTradePrice,
+      }); 
+    } catch (error) {
+      console.error("Failed to make request:", error.message);
+      res.render("submit.ejs", { error: error.message });
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
